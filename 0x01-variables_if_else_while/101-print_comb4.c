10442c63ff89508671033aecfcdb5a6bfdcbb37a
iamstillcath <catherine2lucia@gmail.com>
2023-02-17 15:34:50 -0800

    c commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int c, i;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			if (c < i)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+				putchar(c);
+				putchar(i);
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
+
