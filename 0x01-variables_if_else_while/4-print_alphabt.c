e0bf0bbc60d2bcb9fe92c43c528f8f733068ca9a
iamstillcath <catherine2lucia@gmail.com>
2023-02-17 12:29:37 -0800

    second commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase without q and e
+ * Return: 0 if positive
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10);
+	return (0);
+}
