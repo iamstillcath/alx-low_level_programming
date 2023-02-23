538fae354f6f03cdab115e455dc3d59dc5741d7e
iamstillcath <catherine2lucia@gmail.com>
2023-02-23 15:17:57 -0800

    nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,16 @@
 
 /**
  * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * @x: The character to be checked.
+ * Return: 1 for upper letter or 0 otherwise.
  */
 int _isupper(int x)
+
 {
+
 	if (x >= 65 && x <= 90)
 	{
-		return (1);
+	return (1);
 	}
-		return (0);
+	return (0);
 }
